94ca5d323df0e5f4c093c17a2af8dc72f4721c17
tonyguo <tonyguo2010@gmail.com>
2018-07-22 11:14:09 -0400

    The first stable version.

---
+++ HttpClient/HttpClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HttpClient
+{
+   class HttpClient
+   {
+      private HttpWebRequest request = null;
+      private HttpWebResponse response = null;
+      private String host;
+
+      public HttpClient(String HostURL, CookieContainer Cookies = null, String Method = "GET", String PostBody = "")
+      {
+         host = new Uri(HostURL).Host;
+         request = WebRequest.Create(HostURL) as HttpWebRequest;
+         request.Method = Method;
+         request.ContentType = "application/x-www-form-urlencoded";
+         request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+         request.Accept = "*/*";
+         request.CookieContainer = Cookies;
+
+         //request.Connection = "keep-alive";
+
+         if (Method.Equals("POST") == true)
+         {
+            using (StreamWriter stream = new StreamWriter(request.GetRequestStream()))
+            {
+               stream.Write(PostBody);
+               stream.Close();
+            }
+         }
+         response = request.GetResponse() as HttpWebResponse;
+      }
+
+      ~HttpClient()
+      {
+         response.Close();
+      }
+
+      internal String GetWebContent()
+      {
+         Stream dataStream = response.GetResponseStream();
+
+         StreamReader streamReader = new StreamReader(dataStream);
+         string strContent = streamReader.ReadToEnd();
+         streamReader.Close();
+         dataStream.Close();
+
+         return strContent;
+      }
+
+      internal int GetResponseCode()
+      {
+         return (int)response.StatusCode;
+      }
+   }
+}
---
+++ HttpClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace HttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         CookieContainer SessionInfo = new CookieContainer();
+
+         HttpClient MainPage = new HttpClient("https://www.facebook.com", SessionInfo);
+         MainPage.GetWebContent();
+
+         HttpClient Login = new HttpClient("https://www.facebook.com/login.php?login_attempt=1&lwv=110", SessionInfo, "POST",
+            "Login info");
+         Login.GetWebContent();
+         
+
+      }
+    }
+}
